959bc908d5ca1acecc0bc766151427e202d609e4
behzadiziy <behzadiziy@gmail.com>
2024-11-03 12:55:38 +0330

    notes added to edit card

--- resources/views/livewire/modals/edit-card.blade.php
+++ resources/views/livewire/modals/edit-card.blade.php
@@ -4,7 +4,13 @@
         <div>
             <x-input-label for="title" value="Title" class="sr-only" />
             <x-text-input id="title" placeholder="Card Title" class="w-full" autofocus wire:model="editCardForm.title"/>
-            <x-input-error :messages="$errors->get('editCardForm.title')" />
+            <x-input-error :messages="$errors->get('editCardForm.title')" class="mt-1"/>
+        </div>
+
+        <div>
+            <x-input-label for="notes" value="Note" class="sr-only" />
+            <x-textarea id="notes" placeholder="Card notes" class="w-full" wire:model="editCardForm.notes" rows='6'/>
+            <x-input-error :messages="$errors->get('editCardForm.notes')" class="mt-1"/>
         </div>
 
         <div class="flex items-center justify-between">
@@ -16,7 +22,6 @@
                 Archive
             </x-secondary-button>
         </div>
-
 
     </form>
 </x-modal-wrapper>
