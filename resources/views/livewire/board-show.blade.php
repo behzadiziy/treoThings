5cf5fecfd3ce2dacbda164a2bf50314ec62ec9ce
behzadiziy <behzadiziy@gmail.com>
2024-11-14 13:11:55 +0330

    leave from the board added

--- resources/views/livewire/board-show.blade.php
+++ resources/views/livewire/board-show.blade.php
@@ -1,5 +1,4 @@
 <div>
-
     <x-slot name="header">
         <div class="flex items-center justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -46,6 +45,19 @@
                                     d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                             </svg>
                             <span>Share</span>
+
+                        </x-dropdown-button>
+                    @elseif ($board->users)
+                        <x-dropdown-button class="flex items-center space-x-2"
+                            x-on:click="Livewire.dispatch('openModal' , {component: 'modals.left-from-board', arguments: { 'board': {{ $board->id }} } })">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="size-5 text-red-500">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+                            </svg>
+
+                            <span class="text-red-500">Leave</span>
 
                         </x-dropdown-button>
                     @endif
